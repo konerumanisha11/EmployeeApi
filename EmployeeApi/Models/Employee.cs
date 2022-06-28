780d40fe91e18c0718f16e98b931ee75acc4d4bb
Manisha K <Manisha K@DESKTOP-H22PRKA>
2022-06-28 12:37:04 +0530

    Add project files.

---
+++ EmployeeApi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeApi.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
---
+++ EmployeeApi/Models/EmployeeContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeApi.Models
+{
+    public partial class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.CompanyName)
+                    .HasMaxLength(50)
+                    .HasColumnName("Company Name");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
